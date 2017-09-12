b6bdad6dac7ec0efd9dcf01e1b15986045e26c13
sudarshanamirineni <suda_amir@hotmail.com>
2017-09-13 00:29:55 +0530

    Form Error resolved

--- Teacher/back.php
+++ Teacher/back.php
@@ -8,11 +8,14 @@ $name = "kashyap";
 $class = "TE";
 $division = "C";
 
-
+$date = date("Y/m/d");
 $checked_roll = implode($_POST['roll'], ', ');
 var_dump($checked_roll);
-$count = 5;
-$query = "INSERT INTO `attendance_record` (`subject`, `class`,  `division`, `date`, `count`, `attend_array`) VALUES ('$subject','$class','$division','CURDATE()','$count','$checked_roll')";
+
+$count = explode( ', ', $checked_roll );
+$count1 = count($count);
+
+$query = "INSERT INTO `attendance_record` (`subject`, `class`,  `division`, `date`, `count`, `attend_array`) VALUES ('$subject','$class','$division','$date','$count1','$checked_roll')";
 
 $result = mysqli_query($connect, $query);
 
